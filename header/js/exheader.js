c07dd794fb94e3d0c4f7684f010bb1b51b25a6e3
a791847897 <791847897@qq.com>
2017-01-18 22:08:53 +0800

    Initial commit

---
+++ header/js/exheader.js
@@ -0,0 +1,13 @@
+/**
+ * Created by admin on 2017/1/11.
+ */
+angular.module("fApp").directive("exHeader", function () {
+    return {
+        restrict: "AE",
+        templateUrl: "header/exheader.html",
+        // link:function (scope,iElement,iAttrs,controller) {
+        //
+        // }
+        // require:
+    }
+})
